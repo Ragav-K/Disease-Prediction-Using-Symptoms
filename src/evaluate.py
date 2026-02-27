07d3ba11e796eab37decad40cfd9981c5aef7d7e
Pagalavan <pagalavan06@gmail.com>
2026-02-27 19:06:23 +0530

    completed evaluation

---
+++ src/evaluate.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import joblib
+from sklearn.ensemble import VotingClassifier
+from sklearn.metrics import accuracy_score
+
+
+X_train = pd.read_csv("data/processed/X_train.csv")
+y_train = pd.read_csv("data/processed/y_train.csv").values.ravel()
+X_test = pd.read_csv("data/processed/X_test.csv")
+y_test = pd.read_csv("data/processed/y_test.csv").values.ravel()
+
+
+rf = joblib.load("models/random_forest.pkl")
+dt = joblib.load("models/decision_tree.pkl")
+nb = joblib.load("models/naive_bayes.pkl")
+
+
+ensemble_model = VotingClassifier(
+    estimators=[
+        ("rf", rf),
+        ("dt", dt),
+        ("nb", nb)
+    ],
+    voting="hard"
+)
+
+ensemble_model.fit(X_train, y_train)
+
+y_pred = ensemble_model.predict(X_test)
+
+print("Ensemble Accuracy:", accuracy_score(y_test, y_pred))
+
+joblib.dump(ensemble_model, "models/ensemble_model.pkl")

1312b1755a4cebdde7093bdcf72698920c6e6962
Pagalavan <pagalavan06@gmail.com>
2026-02-28 16:32:28 +0530

    Completed Evaluation

--- src/evaluate.py
+++ src/evaluate.py
@@ -2,14 +2,14 @@ import pandas as pd
 import joblib
 from sklearn.metrics import accuracy_score, classification_report
 
-# Load test data
-X_test = pd.read_csv("X_test.csv")
-y_test = pd.read_csv("y_test.csv").values.ravel()
 
-# Load models
-rf = joblib.load("random_forest.pkl")
-dt = joblib.load("decision_tree.pkl")
-nb = joblib.load("naive_bayes.pkl")
+X_test = pd.read_csv("data/processed/X_test.csv")
+y_test = pd.read_csv("data/processed/y_test.csv").values.ravel()
+
+
+rf = joblib.load("models/random_forest.pkl")
+dt = joblib.load("models/decision_tree.pkl")
+nb = joblib.load("models/naive_bayes.pkl")
 
 models = {
     "Random Forest": rf,

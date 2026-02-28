e67f7a11826f1c1235480dd0b30ab4cab0cffc19
Nilavan-moon <knilavan06@gmail.com>
2026-02-28 12:02:28 +0530

    traine_model

--- src/train_models.py
+++ src/train_models.py
@@ -4,14 +4,14 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
 
-# Load processed data
+
 X_train = pd.read_csv("data/processed/X_train.csv")
 y_train = pd.read_csv("data/processed/y_train.csv").values.ravel()
 
-# Reduce memory usage
+
 X_train = X_train.astype("float32")
 
-# Initialize models (memory optimized)
+
 rf = RandomForestClassifier(
     n_estimators=20,
     max_depth=10,
@@ -22,12 +22,12 @@ rf = RandomForestClassifier(
 dt = DecisionTreeClassifier(max_depth=10, random_state=42)
 nb = GaussianNB()
 
-# Train models
+
 rf.fit(X_train, y_train)
 dt.fit(X_train, y_train)
 nb.fit(X_train, y_train)
 
-# Save models
+
 joblib.dump(rf, "models/random_forest.pkl")
 joblib.dump(dt, "models/decision_tree.pkl")
 joblib.dump(nb, "models/naive_bayes.pkl")

9e2234ba79c12e9268a73426363403e1ce676f77
Nilavan-moon <knilavan06@gmail.com>
2026-02-28 21:00:03 +0530

    trainmodel

--- src/train_models.py
+++ src/train_models.py
@@ -1,35 +1,43 @@
 import pandas as pd
 import joblib
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.naive_bayes import GaussianNB
-
 
 X_train = pd.read_csv("data/processed/X_train.csv")
 y_train = pd.read_csv("data/processed/y_train.csv").values.ravel()
 
-
 X_train = X_train.astype("float32")
 
-
 rf = RandomForestClassifier(
-    n_estimators=20,
-    max_depth=10,
-    n_jobs=1,
+    n_estimators=200,
+    max_depth=None,
+    min_samples_split=2,
+    min_samples_leaf=1,
+    max_features='sqrt',
+    n_jobs=-1,
     random_state=42
 )
 
-dt = DecisionTreeClassifier(max_depth=10, random_state=42)
-nb = GaussianNB()
+dt = DecisionTreeClassifier(
+    max_depth=None,
+    min_samples_split=2,
+    min_samples_leaf=1,
+    random_state=42
+)
 
+gb = GradientBoostingClassifier(
+    n_estimators=100,
+    learning_rate=0.1,
+    max_depth=5,
+    random_state=42
+)
 
 rf.fit(X_train, y_train)
 dt.fit(X_train, y_train)
-nb.fit(X_train, y_train)
-
+gb.fit(X_train, y_train)
 
 joblib.dump(rf, "models/random_forest.pkl")
 joblib.dump(dt, "models/decision_tree.pkl")
-joblib.dump(nb, "models/naive_bayes.pkl")
+joblib.dump(gb, "models/gradient_boosting.pkl")
 
 print("Models Trained & Saved Successfully")
